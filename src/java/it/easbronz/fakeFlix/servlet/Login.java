11ceaf7c53171591b44a2e896f3f29c6ebb67822
Bioparco90 <monnimarco90@gmail.com>
2022-06-15 22:48:10 +0200

    Implementato e testato login
    Sistemare la jsp come da specifiche (un utente loggato, nella pagina di login, vedrà il bottone logout e non più il form di login

--- src/java/it/easbronz/fakeFlix/servlet/Login.java
+++ src/java/it/easbronz/fakeFlix/servlet/Login.java
@@ -1,38 +1,62 @@
 package it.easbronz.fakeFlix.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import it.easbronz.fakeFlix.exceptions.InvalidParamException;
+import it.easbronz.fakeFlix.model.Utente;
+import it.easbronz.fakeFlix.model.UtenteFactory;
+import it.easbronz.fakeFlix.utils.Utils;
 
-@WebServlet(name = "Login", urlPatterns = {"/login"})
+@WebServlet(name = "Login", urlPatterns = { "/login" })
 public class Login extends HttpServlet {
-
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        
-    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
         request.getRequestDispatcher("login.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // processRequest(request, response);
+        HttpSession session = request.getSession();
+        String user = request.getParameter("user");
+        String password = request.getParameter("psw");
+
+        try {
+
+            final int USER_MIN_LENGTH = 5;
+            final int USER_MAX_LENGTH = 20;
+            final int PASSWORD_MIN_LENGTH = 4;
+            final int PASSWORD_MAX_LENGTH = 50;
+
+            Utils.checkString("User", user, USER_MIN_LENGTH, USER_MAX_LENGTH);
+            Utils.checkString("Password", password, PASSWORD_MIN_LENGTH, PASSWORD_MAX_LENGTH);
+
+            Utente utente = UtenteFactory.getInstance().getUtenteByUsernamePassword(user, password);
+
+            if (utente != null) {
+                session.setAttribute("user", utente.getUsername());
+                session.setMaxInactiveInterval(900);
+                response.sendRedirect("catalogo");
+            } else
+                throw new InvalidParamException("Errore Login");
+
+        } catch (InvalidParamException e) {
+            session.invalidate();
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+        }
     }
-    
+
     @Override
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-
 }
