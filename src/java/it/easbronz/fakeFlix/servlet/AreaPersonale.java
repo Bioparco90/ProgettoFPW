f039527b43c818cf98a1e591e76637e09d67ffa2
Bioparco90 <monnimarco90@gmail.com>
2022-06-17 18:06:40 +0200

    Creazione servlet area personale

---
+++ src/java/it/easbronz/fakeFlix/servlet/AreaPersonale.java
@@ -0,0 +1,34 @@
+package it.easbronz.fakeFlix.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "AreaPersonale", urlPatterns = { "/areaPersonale" })
+public class AreaPersonale extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("areaPersonale.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
