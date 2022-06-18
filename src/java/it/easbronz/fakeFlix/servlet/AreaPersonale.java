da6d081f811bd1217f3d0eacb1d8abbddf144e28
Bioparco90 <monnimarco90@gmail.com>
2022-06-18 23:21:56 +0200

    pulizia

--- src/java/it/easbronz/fakeFlix/servlet/AreaPersonale.java
+++ src/java/it/easbronz/fakeFlix/servlet/AreaPersonale.java
@@ -5,8 +5,6 @@
 package it.easbronz.fakeFlix.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Connection;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -16,31 +14,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import it.easbronz.fakeFlix.db.DatabaseManager;
 import it.easbronz.fakeFlix.model.Prodotto;
 import it.easbronz.fakeFlix.model.ProdottoFactory;
 import it.easbronz.fakeFlix.model.Utente;
 import it.easbronz.fakeFlix.model.UtenteFactory;
 
-/**
- *
- * @author monni
- */
-@WebServlet(name = "AreaPersonale", urlPatterns = {"/areaPersonale"})
+@WebServlet(name = "AreaPersonale", urlPatterns = { "/areaPersonale" })
 public class AreaPersonale extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         HttpSession session = request.getSession(false);
         String loggedUser = (String) session.getAttribute("user");
         List<Prodotto> prodotti = ProdottoFactory.getInstance().getAllProdotti(loggedUser);
@@ -50,40 +34,18 @@ public class AreaPersonale extends HttpServlet {
         request.getRequestDispatcher("areaPersonale.jsp").forward(request, response);
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
