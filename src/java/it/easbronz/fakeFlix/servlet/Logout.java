cb18b05ff2612f1f0259a8ee8aa8c2b975c8d1d4
Bioparco90 <monnimarco90@gmail.com>
2022-06-20 17:07:22 +0200

    Aggiunta servlet Logout

---
+++ src/java/it/easbronz/fakeFlix/servlet/Logout.java
@@ -0,0 +1,41 @@
+package it.easbronz.fakeFlix.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(name = "Logout", urlPatterns = {"/logout"})
+public class Logout extends HttpServlet {
+
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session != null){
+            session.invalidate();
+            response.sendRedirect("login");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- web/nuovoProdotto.jsp
+++ web/nuovoProdotto.jsp
@@ -1,4 +1,10 @@
 <%@ page contentType="text/html; charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<c:if test="${empty user}">
+    <c:redirect url="login"/>
+</c:if>
+
 <jsp:include page="header.jsp"/>
   <body>
         <header>
