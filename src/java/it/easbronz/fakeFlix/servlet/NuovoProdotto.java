314265d7f8a9cb21c557c666f9264411b7ef5003
Bioparco90 <monnimarco90@gmail.com>
2022-06-21 15:56:43 +0200

    Gestito controllo sessione

--- src/java/it/easbronz/fakeFlix/servlet/NuovoProdotto.java
+++ src/java/it/easbronz/fakeFlix/servlet/NuovoProdotto.java
@@ -44,50 +44,53 @@ public class NuovoProdotto extends HttpServlet {
         HttpSession session = request.getSession(false);
 
         try {
-            String titolo = request.getParameter("titolo");
-            String trama = request.getParameter("trama");
-            String genere = request.getParameter("genere");
-            String durataPreCheck = request.getParameter("durata");
-            String regista = request.getParameter("regista");
-            String prezzoPreCheck = request.getParameter("prezzo");
-            String uploader = (String) session.getAttribute("user");
+            if (session != null && session.getAttribute("user") != null) {
+                String titolo = request.getParameter("titolo");
+                String trama = request.getParameter("trama");
+                String genere = request.getParameter("genere");
+                String durataPreCheck = request.getParameter("durata");
+                String regista = request.getParameter("regista");
+                String prezzoPreCheck = request.getParameter("prezzo");
+                String uploader = (String) session.getAttribute("user");
 
-            Part file = request.getPart("locandina");
-            String locandina = Utils.getPathImg(file, "products");
+                Part file = request.getPart("locandina");
+                String locandina = Utils.getPathImg(file, "products");
 
-            Utils.checkString("Titolo", titolo, 1, 50);
-            Utils.checkString("Trama", trama, 1, 200);
-            Utils.checkString("Genere", genere, 1, 20);
-            if (!Utils.isInteger("Durata", durataPreCheck)){
-                throw new NumberFormatException("Il campo durata deve contenere un numero intero");
-            }
-            if (!Utils.isFloat("Prezzo", prezzoPreCheck)){
-                throw new NumberFormatException("Il campo prezzo deve contenere un numero decimale");
-            }
-            int durata = Integer.valueOf(durataPreCheck);
-            float prezzo = Float.valueOf(prezzoPreCheck);
-            Utils.checkInteger("Durata", durata, 1, 500);
-            Utils.checkString("Regista", regista, 1, 50);
-            Utils.checkFloat("Prezzo", prezzo);
-            Utils.checkString("Uploader", uploader, 3, 20);
-            Utils.checkString("Locandina", locandina, 1, 200);
+                Utils.checkString("Titolo", titolo, 1, 50);
+                Utils.checkString("Trama", trama, 1, 200);
+                Utils.checkString("Genere", genere, 1, 20);
+                if (!Utils.isInteger("Durata", durataPreCheck)) {
+                    throw new NumberFormatException("Il campo durata deve contenere un numero intero");
+                }
+                if (!Utils.isFloat("Prezzo", prezzoPreCheck)) {
+                    throw new NumberFormatException("Il campo prezzo deve contenere un numero decimale");
+                }
+                int durata = Integer.valueOf(durataPreCheck);
+                float prezzo = Float.valueOf(prezzoPreCheck);
+                Utils.checkInteger("Durata", durata, 1, 500);
+                Utils.checkString("Regista", regista, 1, 50);
+                Utils.checkFloat("Prezzo", prezzo);
+                Utils.checkString("Uploader", uploader, 3, 20);
+                Utils.checkString("Locandina", locandina, 1, 200);
 
-            conn = DatabaseManager.getInstance().getDbConnection();
-            String query = "INSERT INTO prodotto VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-            stmt = conn.prepareStatement(query);
-            stmt.setString(1, titolo);
-            stmt.setString(2, trama);
-            stmt.setString(3, genere);
-            stmt.setInt(4, durata);
-            stmt.setString(5, regista);
-            stmt.setString(6, locandina);
-            stmt.setFloat(7, prezzo);
-            stmt.setString(8, uploader);
-            stmt.executeUpdate();
+                conn = DatabaseManager.getInstance().getDbConnection();
+                String query = "INSERT INTO prodotto VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+                stmt = conn.prepareStatement(query);
+                stmt.setString(1, titolo);
+                stmt.setString(2, trama);
+                stmt.setString(3, genere);
+                stmt.setInt(4, durata);
+                stmt.setString(5, regista);
+                stmt.setString(6, locandina);
+                stmt.setFloat(7, prezzo);
+                stmt.setString(8, uploader);
+                stmt.executeUpdate();
 
-            request.setAttribute("outputMessage", "Prodotto inserito con successo");
-            request.setAttribute("previousPage", "nuovoProdotto");
-            request.getRequestDispatcher("outputPage.jsp").forward(request, response);
+                request.setAttribute("outputMessage", "Prodotto inserito con successo");
+                request.setAttribute("previousPage", "nuovoProdotto");
+                request.getRequestDispatcher("outputPage.jsp").forward(request, response);
+            } else
+                response.sendRedirect("login");
         } catch (NumberFormatException e) {
             request.setAttribute("outputMessage", e.getMessage());
             request.setAttribute("previousPage", "nuovoProdotto");
