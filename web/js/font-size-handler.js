70451a7ac91c7603e5ed14709b160a33e01b52ab
bioparco90 <monnimarco90@gmail.com>
2023-01-05 00:15:36 +0100

    Correzione tabulazione, fregatevene

--- web/js/font-size-handler.js
+++ web/js/font-size-handler.js
@@ -8,31 +8,31 @@ let fontSize = 16;
 // });
 
 $('#decrease-size').click(() => {
-    const smallFontSize = 14;
-    fontSize = smallFontSize;
-    $('body').css('font-size', fontSize + 'px');
-    // localStorage.setItem('fontSize', fontSize);
+	const smallFontSize = 14;
+	fontSize = smallFontSize;
+	$('body').css('font-size', fontSize + 'px');
+	// localStorage.setItem('fontSize', fontSize);
 });
 
 $('#default-size').click(() => {
-    const defaultFontSize = 16;
-    fontSize = defaultFontSize;
-    $('body').css('font-size', fontSize + 'px');
-    // localStorage.setItem('fontSize', fontSize);
+	const defaultFontSize = 16;
+	fontSize = defaultFontSize;
+	$('body').css('font-size', fontSize + 'px');
+	// localStorage.setItem('fontSize', fontSize);
 });
 
 $('#increase-size').click(() => {
-    const bigFontSize = 18;
-    fontSize = bigFontSize;
-    $('body').css('font-size', fontSize + 'px');
-    // localStorage.setItem('fontSize', fontSize);
+	const bigFontSize = 18;
+	fontSize = bigFontSize;
+	$('body').css('font-size', fontSize + 'px');
+	// localStorage.setItem('fontSize', fontSize);
 });
 
 // NB: Alcune cose non sono perfettamente responsive, quindi si notano sovrapposizioni
-// tipo l'utilissimo tasto "acquista" che va sotto la card con testo grande. 
+// tipo l'utilissimo tasto "acquista" che va sotto la card con testo grande.
 // Ciò può essere risolto settando 12- 14 -16 come sizes, ma il default normalmente
 // è 16, inoltre si otterrebbe un testo abbastanza piccolo in certe pagine, sicuramente
 // più piccolo di quanto vorremmo. Vedete voi il da farsi, dell'aspetto mi fotte fino a pagina 2.
 
-// Le linee commentate servono a garantire la persistenza della scelta, 
+// Le linee commentate servono a garantire la persistenza della scelta,
 // come al solito prima implemento e poi leggo le specifiche.
