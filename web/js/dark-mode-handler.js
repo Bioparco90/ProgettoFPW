ba6dffcfb18b2600fb897f3707927ddb55cb0388
bioparco90 <monnimarco90@gmail.com>
2023-01-04 19:52:05 +0100

    Commentato il codice relativo alla dark mode

--- web/js/dark-mode-handler.js
+++ web/js/dark-mode-handler.js
@@ -1,17 +1,16 @@
-const toggle = $('#toggle');
-
-// Check if the user has a saved preference for dark mode
+// Controlla la cache in cerca di un'eventuale preferenza pregressa
 const isDarkMode = localStorage.getItem('dark-mode');
 
-// If the user has a saved preference and it is set to dark mode,
-// add the `dark-mode` class to the `<body>` element to enable dark mode
+// Se l'utente ha una preferenza salvata e questa è impostata sulla modalità dark, 
+// aggiungi la classe "dark-mode" al body per abilitare la modalità scura.
+// In prima istanza, non essendoci ancora una preferenza, il sito partirà in light mode.
 if (isDarkMode === 'true') {
   $('body').addClass('dark-mode');
 }
 
-// When the toggle is clicked, toggle the dark mode class on the body
-// and save the preference to local storage
-toggle.click(() => {
+// Al click del tasto apposito, avviene il passaggio da una modalità all'altra.
+// Infine viene salvata la nuova preferenza in cache.
+$('#toggle').click(() => {
   $('body').toggleClass('dark-mode');
   localStorage.setItem('dark-mode', $('body').hasClass('dark-mode'));
 });
